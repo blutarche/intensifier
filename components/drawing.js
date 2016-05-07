9ba0e11d960cbfbcdd2a61d63de250560a3f1405
Supanut <supanut.overload@gmail.com>
2016-05-08 00:24:18 +0700

    vibration

--- components/drawing.js
+++ components/drawing.js
@@ -1,26 +1,43 @@
 var randomMax = 10;
+var shiftPosition = 0;
+var middleRange = 0;
+var img = new Image();
 
 var Drawing = React.createClass({
   getInitialState: function() {
-    return {url: 'some-url'};
+    return {
+      url: 'some-url-image'
+    };
   },
 
   componentDidMount: function() {
-    var img = new Image();
     img.src = this.state.url;
-    this.updateCanvas();
+    middleRange = randomMax / 2;
+    var canvas = this.refs.canvas;
+    canvas.width = img.width - randomMax;
+    canvas.height = img.height - randomMax;
+    setInterval(this.updatePosition, 10);
   },
 
-  updateCanvas: function() {
-    var ctx = this.refs.canvas.getContext('2d');
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 300, 300);
+  updatePosition: function() {
+    var canvas = this.refs.canvas;
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;  
+    var shiftPosition = Math.floor((Math.random() * randomMax) - middleRange);
+    var x = -middleRange + shiftPosition;
+    var y = -middleRange + shiftPosition;
+    var w = width - middleRange;
+    var h = height - middleRange;
+    ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(img, x, y);
   },
 
   render: function() {
-    return <canvas ref="canvas" width={300} height={300}/>
+    return <canvas ref="canvas" width={300} height={300} />
   }
 });
+
 ReactDOM.render(
   <Drawing />,
   document.getElementById('drawing')
