c420d7b0d6891faaee43b449d5720b850f852104
blutarche <aikdanai.s@gmail.com>
2016-05-08 03:59:06 +0700

    Display file name when uploaded

--- components/fileinput.js
+++ components/fileinput.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+var nameStyle = {
+  display: "none"
+};
+
 export default class FileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.fileChange = this.fileChange.bind(this);
+  }
+  fileChange() {
+    console.log('Selected file:', event.target.files[0]);
+    alert("f");
+  }
   render() {
     return (
       <div className="text-center">
-  		  <button className="btn btn-hg btn-embossed btn-primary upload">
-  			<i className="fa fa-picture-o" aria-hidden="true"></i> Upload your own photo
-  			<input type="file" name="picture" accept="image/*" />
-  		  </button>
+        <div className="fileName" style={nameStyle}>
+          <b>Current picture:</b> <span></span>
+        </div>
+        <button className="btn btn-hg btn-embossed btn-primary upload">
+          <i className="fa fa-picture-o" aria-hidden="true"></i> Upload youre own photo
+          <input ref="photo" type="file" name="picture" accept="image/*" onChange={this.fileChange} />
+        </button>
       </div>
     );
   }
