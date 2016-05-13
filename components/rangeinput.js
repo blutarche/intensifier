cca3e722a5e6952feca1914e659639cb2fe3459c
Supanut <supanut.overload@gmail.com>
2016-05-13 17:36:06 +0700

    RangeInput Style + Label #11

--- components/rangeinput.js
+++ components/rangeinput.js
@@ -4,23 +4,17 @@ import React from 'react';
 export default class RangeInput extends React.Component {
   constructor() {
     super();
-    // this.fileChange = this.fileChange.bind(this);
   }
 
-  // fileChange(e) {
-  //   // var reader = new FileReader();
-  //   // var file = e.target.files[0];
-  //   // var url = URL.createObjectURL(file);
-  //   // console.log("fileChange URL: "+url);
-  //   // this.props.updatePicture(url);
-  //   // window.file = file;
-  // }
-
   render() {
+    console.log(this.props.labelText);
     return (
       <div className={this.props.shouldShow ? "form-group" : "hidden"}>
-        <div className="col-sm-5 col-sm-offset-4">
-          <input className={this.props.shouldShow ? "form-group" : "hidden"} type="range" min="0" max="15" onChange={this.props.rangeChange.bind(this)} />
+        <label className="col-sm-4 control-label"> 
+          <b>{this.props.labelText}</b>
+        </label>
+        <div className="text-center col-sm-5">
+          <input className={this.props.shouldShow ? "form-group" : "hidden"} type="range" min="0" max="15" onChange={this.props.rangeChange.bind(this)} style={{"marginLeft": "0", "marginRight": "0"}} />
         </div>
       </div>
     )
